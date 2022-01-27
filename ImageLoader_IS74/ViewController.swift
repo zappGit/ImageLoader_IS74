a005e1cf0182931b89953128ef084f85acf59e65
Artyom Khrebtov <t.khrebtov@yandex.ru>
2022-01-27 16:56:02 +0500

    Initial Commit

---
+++ ImageLoader_IS74/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLoader_IS74
+//
+//  Created by Артем Хребтов on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
